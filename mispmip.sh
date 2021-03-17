809e60156d30f409d182989ddf48edad37ce25d0
Xavier Gillard <xavier.gillard@uclouvain.be>
2021-03-17 10:02:24 +0100

    make runscript more generic

--- mispmip.sh
+++ mispmip.sh
@@ -1,4 +1,17 @@
 :
 
+###############################################################################
+## On my local machine that would be:
+###############################################################################
+# export GUROBI_HOME="/home/xgillard/gurobi903/linux64/lib"
+# export CLASSPATH="${GUROBI_HOME}/*:/home/xgillard/mispmip/mispmip.jar"
+# export PATH=$PATH:$GUROBI_HOME:.
+# export LD_LIBRARY_PATH="${GUROBI_HOME}"
+###############################################################################
 
-java -cp '/Library/gurobi903/mac64/lib/*:./mispmip.jar' com.github.xgillard.mispmip.Main $@
+export GUROBI_HOME="/home/xgillard/gurobi903/linux64/lib"
+export CLASSPATH="${GUROBI_HOME}/*:/home/xgillard/mispmip/mispmip.jar"
+export PATH=$PATH:$GUROBI_HOME:.
+export LD_LIBRARY_PATH="${GUROBI_HOME}"
+
+java -Djava.library.path="${GUROBI_HOME}" -cp "${CLASSPATH}" com.github.xgillard.mispmip.Main $@
